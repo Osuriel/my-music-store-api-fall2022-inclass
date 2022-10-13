cc8007d85ac515932acb41a5b9d2c7994d7b4137
Omar Suriel <omarsuriel@icloud.com>
2022-10-13 14:52:43 -0400

    User Auth Middleware

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const cookieParser = require('cookie-parser');
 var cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const UserModel = require('./models/UserModel');
 const userRouter = require('./routes/userRouter');
+const jwt = require('jsonwebtoken');
+
 
 const port = process.env.PORT;
 
@@ -23,6 +26,38 @@ app.use(cookieParser());
 
 // Parsing JSON to req.body
 app.use(bodyParser.json());
+
+//Authorization Middleware
+app.use(async (req, res, next) => {
+  try {
+      const { session_token: sessionToken } = req.cookies;
+      
+      if(!sessionToken){
+        return next();
+      }
+    
+      // This returns the data in the JWT or it throws if the jwt is not valid
+      const { userId, iat } = jwt.verify(sessionToken, process.env.AUTH_SECRET_KEY);
+    
+      // if the token is too old we reject it.
+      if( iat < (Date.now() - (30 * 24 * 60 * 60 * 1000))){
+        return res.status(401).send("Session expired")
+      }
+    
+      const foundUser = await UserModel.findOne( { _id: userId }); 
+      
+      if(!foundUser){
+        return next();
+      }
+    
+      // after we find the user in the token we add it to the request
+      req.user = foundUser;
+    
+      return next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 // EXPRESS MIDDLEWARES:
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -14,7 +14,28 @@ const cleanUser = (userDocument) => {
   }
 }
 
+const getToken = (userId) => {
+  return jwt.sign({ userId, iat: Date.now() }, process.env.AUTH_SECRET_KEY);
+};
+
 const userRouter = express.Router();
+
+
+// Route to test user auth
+userRouter.get('/test-auth', async (req, res, next) => {
+
+  try {
+
+    // If the user has not logged in. return an error.
+    if(!req.user){
+      return res.status(403).send("User not logged in")
+    }
+  
+    res.send({ userFirstName: req.user.firstName });
+  } catch (error){
+    next(error);
+  }
+});
 
 // Route to create user accounts
 userRouter.post('/register-user', async (req, res, next) => {
@@ -25,14 +46,23 @@ userRouter.post('/register-user', async (req, res, next) => {
   
   // HASH (Bcrypt) the password
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  
   
   try {    
     // store a password hash, not the password
     const userDocument = new UserModel({
       firstName, lastName, email, hashedPassword, profilePicture
     });
-  
+
     await userDocument.save();
+
+    const token = getToken(userDocument._id);
+    
+    res.cookie('session_token', token, {
+      httpOnly: true,
+      // Should be true when using https a.k.a. in prod.
+      secure: false })
     
     res.send({ user: cleanUser(userDocument) })
   } catch (error) {
@@ -67,7 +97,7 @@ userRouter.post('/sign-in', async (req, res, next) => {
     
     // The user can be successfully authenticated
     // Send user data back to client
-    const token = jwt.sign({ userId: foundUser._id, iat: Date.now() }, process.env.AUTH_SECRET_KEY);
+    const token = getToken(foundUser._id);
     
     res.cookie('session_token', token, {
       httpOnly: true,
